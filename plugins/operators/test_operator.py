997abc964791fb182e97c55f0dd674f7ea83a8b1
Manuel Amador <manuel.amador@dfinity.org>
2023-07-04 17:35:20 +0200

    Test that the DAG successfully imports when the module gets another class added.

--- plugins/operators/test_operator.py
+++ plugins/operators/test_operator.py
@@ -10,3 +10,8 @@ from airflow.utils.context import Context
 class TestTask(BaseOperator):
     def execute(context: Context) -> None:  # type:ignore
         print("Test task successfully executed.")
+
+
+class TestTask2(BaseOperator):
+    def execute(context: Context) -> None:  # type:ignore
+        print("Test task 2 successfully executed.")
