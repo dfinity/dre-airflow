3bbb4181652ed0230af97b2cafd2e7c7012a4564
Pietro <pietro.di.marco@dfinity.org>
2023-07-14 12:16:34 +0200

    Test static

--- shared/dfinity/ic_os_rollout.py
+++ shared/dfinity/ic_os_rollout.py
@@ -3,7 +3,7 @@ Rollout IC os to subnets.
 """
 
 import datetime
-from typing import Any, Callable
+from typing import Any, Callable, List, Dict
 
 from dfinity.ic_types import SubnetRolloutInstance
 
@@ -13,7 +13,7 @@ SLACK_CONNECTION_ID = "slack.ic_os_rollout"
 
 def week_planner(now: datetime.datetime | None = None) -> dict[str, datetime.datetime]:
     if now is None:
-        now = datetime.datetime.now()
+        now = datetime.datetime.utcnow()
     days = {
         "Monday": now - datetime.timedelta(days=now.weekday()),
         "Tuesday": now - datetime.timedelta(days=now.weekday() - 1),
@@ -91,4 +91,62 @@ def rollout_planner(
                     )
             for sn in subnet_numbers:
                 res.append(SubnetRolloutInstance(date_and_time, sn, subnet_list[sn]))
+    return res
+
+
+def rollout_planner_static(
+    plan: dict[str, dict[str, List[int]]],
+    subnet_list_source: Callable[[], list[str]],
+    now: datetime.datetime | None = None,
+) -> Dict[str, List[SubnetRolloutInstance]]:
+    res = dict()
+    subnet_list = subnet_list_source()
+    week_plan = week_planner(now)
+    for dayname, hours in plan.items():
+        try:
+            date = week_plan[dayname]
+        except KeyError:
+            raise ValueError(f"{dayname} is not a valid day")
+        for time_s, subnet_numbers in hours.items():
+            org_time_s = time_s
+            if isinstance(time_s, int):
+                hour = int(time_s / 60)
+                minute = time_s % 60
+                time_s = f"{hour}:{minute}"
+            try:
+                time = datetime.datetime.strptime(time_s, "%H:%M")
+            except Exception as exc:
+                raise ValueError(
+                    f"{org_time_s} is not a valid hh:mm time on {dayname}"
+                ) from exc
+            date_and_time = date.replace(hour=time.hour, minute=time.minute)
+            date_and_time_str = date_and_time.strftime("%A_%B_%d__%Y_%I_%M_%S_%p")
+
+            for n, sn in enumerate(subnet_numbers):
+                if isinstance(sn, str):
+                    prefix_matches = [
+                        (m, s) for m, s in enumerate(subnet_list) if s.startswith(sn)
+                    ]
+                    if not prefix_matches:
+                        raise ValueError(
+                            f"subnet specification {sn} not in known subnet list"
+                            " for this network"
+                        )
+                    if len(prefix_matches) > 1:
+                        raise ValueError(f"subnet specification {sn} is ambiguous")
+                    subnet_numbers[n] = prefix_matches[0][0]
+                elif not isinstance(sn, int) or sn < 0:
+                    raise ValueError(
+                        f"subnet number {sn} in {hours} of {dayname} is"
+                        " not a zero/positive integer or a valid subnet ID"
+                    )
+            for sn in subnet_numbers:
+                if sn >= len(subnet_list):
+                    raise ValueError(
+                        f"subnet number {sn} in {hours} of {dayname} is"
+                        " larger than the known total number of subnets"
+                    )
+            
+            for sn in subnet_numbers:
+                res[date_and_time_str].append(SubnetRolloutInstance(date_and_time, sn, subnet_list[sn]))
     return res
