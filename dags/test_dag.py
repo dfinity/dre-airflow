d45afd3f61aaadd836efd4b403357c560ebc44ba
Manuel Amador <manuel.amador@dfinity.org>
2023-08-29 18:37:31 +0200

    Add a task that logs.

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -8,6 +8,7 @@ import operators.test_operator as test_operator
 import pendulum
 
 from airflow import DAG
+from airflow.decorators import task
 
 with DAG(
     dag_id="test_dag",
@@ -17,6 +18,11 @@ with DAG(
     dagrun_timeout=datetime.timedelta(days=14),
     tags=["testing"],
 ) as dag:
+
+    @task
+    def finish() -> None:
+        print("Finished!")
+
     (
         test_operator.TestTask(
             task_id="task_1",
@@ -30,4 +36,5 @@ with DAG(
         >> test_operator.TestTask(
             task_id="task_4",
         )
+        >> finish()
     )
