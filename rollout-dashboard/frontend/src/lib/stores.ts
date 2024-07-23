b5c96a15017668b7e99b2c9f0c9e7fb1d696bbed
Manuel Amador <manuel.amador@dfinity.org>
2024-07-23 18:58:33 +0200

    Distinguish between frontend and backend errors.

--- rollout-dashboard/frontend/src/lib/stores.ts
+++ rollout-dashboard/frontend/src/lib/stores.ts
@@ -27,13 +27,18 @@ export const rollout_query = (() => {
         } else {
             // Sometimes the API will fail!
             // FIXME: we should handle this with an error shown to the user.
-            console.log('Request for rollout data failed: ' + res.ok)
             let responseText = await res.text()
             let errorText = res.status + " " + res.statusText
             if (responseText) {
                 responseText = responseText.split("\n")[0]
                 errorText = errorText + ": " + responseText
+            } else if (res.status == 500) {
+                // An HTTP 500 error without status text from fetch() indicates
+                // that the fetch() call could not contact the backend server,
+                // rather than an HTTP error proper coming from the backend.
+                errorText = "not possible to contact rollout backend (network issues / backend down)"
             }
+            console.log('Request for rollout data failed: ' + errorText)
             store.set({
                 rollouts: get(store).rollouts,
                 error: errorText
