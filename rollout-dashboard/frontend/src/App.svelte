b429e165647c7ce49e3eb6aef52b5957dd1b177d
Manuel Amador <manuel.amador@dfinity.org>
2024-07-23 19:14:43 +0200

    Add padding right of the error icon to ensure the text of the warning is not too close to the icon.

--- rollout-dashboard/frontend/src/App.svelte
+++ rollout-dashboard/frontend/src/App.svelte
@@ -22,8 +22,9 @@
 </nav>
 
 {#if $my_rollout_query.error && $my_rollout_query.error !== "loading"}
+<!-- note use of me-3 in svg icon to ensure icon actually shows not too stuck to the text -->
 <div class="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
-  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <svg class="w-6 h-6 me-3 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
     <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v5a1 1 0 1 0 2 0V8Zm-1 7a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H12Z" clip-rule="evenodd"/>
   </svg>
   <span class="sr-only">Error</span>
