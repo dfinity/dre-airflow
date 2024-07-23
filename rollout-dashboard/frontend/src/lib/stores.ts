c3fa9e1b71b72120ddd8df6d190dfd733bfc40dd
Manuel Amador <manuel.amador@dfinity.org>
2024-07-23 19:21:05 +0200

    Handle backend timeouts on the client side.
    Timeouts on the client side should only happen when there is a networking issue, since the server responds instantly due to it serving cached data.

--- rollout-dashboard/frontend/src/lib/stores.ts
+++ rollout-dashboard/frontend/src/lib/stores.ts
@@ -1,5 +1,7 @@
 import { get, writable } from 'svelte/store'
 
+const BACKEND_TIMEOUT = 15000
+
 type RolloutResult = {
     error: string | Object | null;
     rollouts: Array<Object>;
@@ -11,33 +13,43 @@ export const rollout_query = (() => {
     let updater = async () => {
         const API_URL = import.meta.env.BACKEND_API_PATH || "/api/v1";
         const url = API_URL + "/rollouts"
-        const res = await fetch(url);
-        if (res.ok) {
-            if (res.status == 204) {
-                console.log('Data is not yet available.  Retrying soon.')
-                store.set({ rollouts: [], error: "loading" })
+        try {
+            const res = await fetch(url, { signal: AbortSignal.timeout(BACKEND_TIMEOUT) });
+            if (res.ok) {
+                if (res.status == 204) {
+                    console.log('Data is not yet available.  Retrying soon.')
+                    store.set({ rollouts: [], error: "loading" })
+                } else {
+                    let json = await res.json()
+                    store.set({
+                        rollouts: json,
+                        error: null
+                    })
+                }
+                setTimeout(updater, 5000)
             } else {
-                let json = await res.json()
+                // Sometimes the API will fail!
+                // FIXME: we should handle this with an error shown to the user.
+                let responseText = await res.text()
+                let errorText = res.status + " " + res.statusText
+                if (responseText) {
+                    responseText = responseText.split("\n")[0]
+                    errorText = errorText + ": " + responseText
+                } else if (res.status == 500) {
+                    // An HTTP 500 error without status text from fetch() indicates
+                    // that the fetch() call could not contact the backend server,
+                    // rather than an HTTP error proper coming from the backend.
+                    errorText = "not possible to contact rollout backend (network issues / backend down)"
+                }
+                console.log('Request for rollout data failed: ' + errorText)
                 store.set({
-                    rollouts: json,
-                    error: null
+                    rollouts: get(store).rollouts,
+                    error: errorText
                 })
+                setTimeout(updater, 15000)
             }
-            setTimeout(updater, 5000)
-        } else {
-            // Sometimes the API will fail!
-            // FIXME: we should handle this with an error shown to the user.
-            let responseText = await res.text()
-            let errorText = res.status + " " + res.statusText
-            if (responseText) {
-                responseText = responseText.split("\n")[0]
-                errorText = errorText + ": " + responseText
-            } else if (res.status == 500) {
-                // An HTTP 500 error without status text from fetch() indicates
-                // that the fetch() call could not contact the backend server,
-                // rather than an HTTP error proper coming from the backend.
-                errorText = "not possible to contact rollout backend (network issues / backend down)"
-            }
+        } catch (e) {
+            let errorText = "the request to the backend has timed out (network issues / backend under high load)"
             console.log('Request for rollout data failed: ' + errorText)
             store.set({
                 rollouts: get(store).rollouts,
