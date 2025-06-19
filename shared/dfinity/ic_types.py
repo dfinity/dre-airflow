cf7aed984434fbed7821a15a03c3e1cd48a21589
DFINITYManu <123385598+DFINITYManu@users.noreply.github.com>
2025-06-19 12:44:53 +0200

    Use a correct node count to match the number of nodes being updated in the sample proposal.
    Co-authored-by: r-birkner <103420898+r-birkner@users.noreply.github.com>

--- shared/dfinity/ic_types.py
+++ shared/dfinity/ic_types.py
@@ -254,10 +254,10 @@ class AbbrevBoundaryNodesUpdateProposal(TypedDict):
                 'node_ids': ['4fssn-4vi43-2qufr-hlrfz-hfohd-jgrwc-7l7ok-uatwb-ukau7-lwmoz-tae']},
     'proposer': '80',
     'status': 'EXECUTED',
-    'summary': 'Update 9 API boundary node(s) to '
+    'summary': 'Update 1 API boundary node(s) to '
                 'f8131bfbc2d339716a9cff06e04de49a68e5a80b '
                 '\n'nMotivation...',
-    'title': 'Update 9 API boundary node(s) to f8131bf',
+    'title': 'Update 1 API boundary node(s) to f8131bf',
     'topic': 'TOPIC_IC_OS_VERSION_DEPLOYMENT'}
     """
 
