5762add695a69997666f768e3912e7f1855dfa07
Manuel Amador <manuel.amador@dfinity.org>
2025-07-23 15:22:09 +0200

    Initial use of Routify v3 to provide multiple views in the rollout dashboard.

--- rollout-dashboard/frontend/vite.config.ts
+++ rollout-dashboard/frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
+import routify from '@roxi/routify/vite-plugin'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 const BASE_URL = 'http://localhost:4174';
 
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [
+		svelte(),
+		routify({})
+	],
 	server: {
 		proxy: {
 			'/api': { target: BASE_URL },
